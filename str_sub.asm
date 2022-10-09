39e8b7655bec93bb489a92d28eec48af7a69fad2
RechieKho <the.real.richie.kho@gmail.com>
2022-10-09 17:54:51 +0800

    Add strn_is_uint and strn_to_uint function for conversion.

--- str_sub.asm
+++ str_sub.asm
@@ -30,4 +30,83 @@ str_has_char:
 	popa
 	ret
 
+; check is string with n length unsigned interger
+; si <- string 
+; cx <- length 
+; cf -> set if it is an unsigned interger
+strn_is_uint:
+	pusha 
+	cmp cx, 0 
+	je .not_uint
+.loop: 
+	mov byte al, [si]
+	cmp al, '0'
+	jb .not_uint 
+	cmp al, '9'
+	ja .not_uint
+	dec cx 
+	jnz .loop
+	jmp .is_uint
+.not_uint:
+	stc
+.is_uint:
+	popa 
+	ret
+
+; convert string with n length to unsigned interger 
+; si <- string 
+; cx <- length
+; dx -> unsigned interger
+; cf -> set if it is invalid (not unsigned int or too big)
+strn_to_uint:
+	push ax 
+	push bx 
+	push cx 
+	push si 
+	push di
+	call strn_is_uint
+	jnc .invalid_uint
+	cmp cx, 5 
+	ja .invalid_uint
+	xor bx, bx 
+	xor di, di
+	add si, cx
+	dec si 
+.char_loop:
+	xor ax, ax
+	mov byte al, [si]
+	sub ax, '0'
+	push cx
+	push bx
+	xor dx, dx
+	mov cx, 10
+.mul_loop:
+	cmp bx, 0 
+	je .mul_loop_end
+	mul cx
+	dec bx
+	jmp .mul_loop
+.mul_loop_end:
+	pop bx
+	pop cx
+	cmp dx, 0 
+	jne .invalid_uint
+	add di, ax ; using di as sum temporarily because running out of register
+	jc .invalid_uint
+	inc bx
+	dec si
+	dec cx 
+	jnz .char_loop
+	jmp .success
+.invalid_uint:
+	stc
+.success:
+	mov dx, di
+	pop di 
+	pop si 
+	pop cx 
+	pop bx 
+	pop ax
+	ret
+
 %endif ; _STR_SUB_ASM_
